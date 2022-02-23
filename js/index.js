45e866802b0a856453a34ae1f97f7aa79b36dc51
Shariare-Islam <animshariare42@gmai.com>
2022-02-23 21:08:04 +0600

    second

--- js/index.js
+++ js/index.js
@@ -293,7 +293,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/gbICivOO26U?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/watch?v=yAO9rPZcJBY");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
